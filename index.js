dc8d0bdc3e0487157377746ea834ac51b1a74db1
Kartik <kk8923423@gmail.com>
2025-12-07 20:59:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const varibale = 'variable'
+console.log(varibale)
+
+const header = 'header'
+console.log(header)
+
+const footer = 'footer'
+console.log(footer)
+
+const herosection = 'hero section'
+console.log(herosection)
