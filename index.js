4b8ba694dd2187bc014e7c53f9799389905f7e27
Kartik <kk8923423@gmail.com>
2025-12-08 18:31:57 +0530

    third commit

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ console.log(footer)
 
 const herosection = 'hero section'
 console.log(herosection)
+
+const padding = 'padding'
+console.log(padding)
+
+const bopper = 'booper'
+console.log(bopper)
+
+const lost = 'lost'
+console.log(lost)
+
+const hello = 'hello'
+console.log(hello)
